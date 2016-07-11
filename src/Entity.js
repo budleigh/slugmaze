fcda9fe5b2384d7b3be51c073da57774b471b1d5
graftss <coughcoughcough@gmail.com>
2016-07-11 16:01:37 -0400

    add center coordinates `cx` and `cy` to `Entity`

--- src/Entity.js
+++ src/Entity.js
@@ -4,16 +4,25 @@ class Entity {
     this.y = y;
     this.w = w;
     this.h = h;
+
+    this.computeCenter();
+  }
+
+  computeCenter() {
+    this.cx = this.x + this.w / 2;
+    this.cy = this.y + this.h / 2;
   }
 
   moveBy(dx, dy) {
     this.x += dx;
     this.y += dy;
+    this.computeCenter();
   }
 
   moveTo(x, y) {
     this.x = x;
     this.y = y;
+    this.computeCenter();
   }
 
   // empty placeholder methods so we don't have to
