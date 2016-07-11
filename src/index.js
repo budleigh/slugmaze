b15ecf175b4e075308b5eaedf144b59aa6f8f10c
graftss <coughcoughcough@gmail.com>
2016-07-11 15:24:22 -0400

    add basic `gameloop`

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import str from './other-file.js';
+import Input from './input.js';
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var width = 800;
+var height = 600;
 
-ctx.fillStyle = str;
-ctx.fillRect(30, 30, 30, 30);
+var x = 30;
+
+function update(dt) {
+  x += 3;
+}
+
+function draw(ctx) {
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillRect(x, 20, 30, 30);
+}
+
+let lastTimestamp;
+let nextTimestamp;
+
+function gameloop(timestamp) {
+  lastTimestamp = nextTimestamp;
+  nextTimestamp = timestamp;
+
+  update((nextTimestamp - lastTimestamp) / 1000);
+  draw(ctx);
+
+  requestAnimationFrame(gameloop);
+}
+
+requestAnimationFrame(gameloop);
