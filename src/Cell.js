3e336c9d49709a287579da5605b0ba9fd2c6c578
graftss <coughcoughcough@gmail.com>
2016-07-11 16:20:51 -0400

    add `drawCenter` to `Cell`

--- src/Cell.js
+++ src/Cell.js
@@ -3,6 +3,21 @@ import Entity from './Entity.js';
 class Cell extends Entity {
   constructor(x, y, w, h) {
     super(x, y, w, h);
+    this.centerSize = 6;
+  }
+
+  drawCenter(ctx) {
+    ctx.save();
+
+    ctx.fillStyle = 'rgb(180,180,180)';
+    ctx.fillRect(
+      -this.centerSize / 2,
+      -this.centerSize / 2,
+      this.centerSize,
+      this.centerSize
+    );
+
+    ctx.restore();
   }
 
   draw(ctx) {
@@ -12,6 +27,12 @@ class Cell extends Entity {
 
     ctx.fillRect(0, 0, this.w, this.h);
 
+
+    ctx.restore();
+
+    ctx.save();
+    ctx.translate(this.cx, this.cy);
+    this.drawCenter(ctx);
     ctx.restore();
   }
 }
